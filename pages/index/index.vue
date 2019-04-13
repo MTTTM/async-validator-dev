c8c0e7e8406f80799d7a2541ab0b6d429a48f3d7
boolean <boolean@163.com>
2019-04-13 15:04:21 +0800

    添加table 组件

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,113 +3,136 @@
 		<view class="uni-header-logo">
 			<image src="../../../static/componentIndex.png"></image>
 		</view>
-		<view class="uni-hello-text uni-common-pb">
-			以下将展示async-validator-uniapp组件
-		</view>
-		<view class="uni-card" v-for="(list,index) in lists" :key="index">
+		<view class="uni-hello-text uni-common-pb">以下将展示async-validator-uniapp组件</view>
+		<view class="uni-card" v-for="(list, index) in lists" :key="index">
 			<view class="uni-list">
 				<view class="uni-list-cell uni-collapse">
-					<view class="uni-list-cell-navigate uni-navigate-bottom" hover-class="uni-list-cell-hover" :class="list.open ? 'uni-active' : ''"
-					 @click="triggerCollapse(index)">
-						{{list.name}}
+					<view
+						class="uni-list-cell-navigate uni-navigate-bottom"
+						hover-class="uni-list-cell-hover"
+						:class="list.open ? 'uni-active' : ''"
+						@click="triggerCollapse(index)"
+					>
+						{{ list.name }}
 					</view>
 					<view class="uni-list uni-collapse" :class="list.open ? 'uni-active' : ''">
-						<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,key) in list.pages" :key="key" @click="goDetailPage(item)">
-							<view class="uni-list-cell-navigate uni-navigate-right"> {{item.label}} </view>
+						<view
+							class="uni-list-cell"
+							hover-class="uni-list-cell-hover"
+							v-for="(item, key) in list.pages"
+							:key="'input'+key"
+							@click="goDetailPage(item)"
+						>
+							<view class="uni-list-cell-navigate uni-navigate-right">
+								{{ item.label }}
+							</view>
 						</view>
 					</view>
 				</view>
 			</view>
-		</view>
+	    </view>
+
 	</view>
 </template>
 <script>
-	export default {
-		data() {
-			return {
-				lists: [{
-						id: 'form',
-						name: '表单组件',
+export default {
+	data() {
+		return {
+			lists: [{
+					id: 'form',
+					name: '表单组件',
+					open: false,
+					pages: [
+					{
+						url:"form/x-form-item/x-form-item",
+						label:"最简单的校验"
+					},
+					{
+						url:"form/x-form-item/x-form-item-error",
+						label:"最推荐"
+					},
+					{
+						url:"form/form",
+						label:"旧的/@input同步"
+					},
+					{
+						url:"form/decreateInput",
+						label:"改进/动态增加"
+					},
+					{
+						url:"form/singleForm",
+						label:'改进/自定义错误&&成功样式'
+					},
+					{
+						url:"form/singleToat",
+						label:'改进/toast提示，顺序校验'
+					},
+					{
+						url:"form/toastdecreateInput",
+						label:'toast&&增减'
+					},
+					//
+						{
+						url:"form/singleRadioCheckbox",
+						label:'checkbox&&radio'
+					},
+					]
+				},
+				{
+						id: 'table',
+						name: 'table',
 						open: false,
 						pages: [
-						{
-							url:"form/x-form-item/x-form-item",
-							label:"最简单的校验"
-						},
-						{
-							url:"form/x-form-item/x-form-item-error",
-							label:"最推荐"
-						},
-						{
-							url:"form/form",
-							label:"旧的/@input同步"
-						},
-						{
-							url:"form/decreateInput",
-							label:"改进/动态增加"
-						},
-						{
-							url:"form/singleForm",
-							label:'改进/自定义错误&&成功样式'
-						},
-						{
-							url:"form/singleToat",
-							label:'改进/toast提示，顺序校验'
-						},
-						{
-							url:"form/toastdecreateInput",
-							label:'toast&&增减'
-						},
-						//
 							{
-							url:"form/singleRadioCheckbox",
-							label:'checkbox&&radio'
-						},
+								url:"table/table",
+								label:"table"
+							}
 						]
 					}
-				]
+			]
+			
+		}
+	},
+
+
+	methods: {
+		triggerCollapse(e) {
+			if (!this.lists[e].pages) {
+				this.goDetailPage(this.lists[e].url);
+				return;
+			}
+			for (var i = 0; i < this.lists.length; ++i) {
+				if (e === i) {
+					this.lists[i].open = !this.lists[e].open;
+				} else {
+					this.lists[i].open = false;
+				}
 			}
 		},
-
-
-		methods: {
-			triggerCollapse(e) {
-				if (!this.lists[e].pages) {
-					this.goDetailPage(this.lists[e].url);
-					return;
-				}
-				for (var i = 0; i < this.lists.length; ++i) {
-					if (e === i) {
-						this.lists[i].open = !this.lists[e].open;
-					} else {
-						this.lists[i].open = false;
-					}
-				}
-			},
-			goDetailPage(e) {
-					uni.navigateTo({
-						url: "/pages/"+e.url
-					})
-			}
+		goDetailPage(e) {
+				uni.navigateTo({
+					url: "/pages/"+e.url
+				})
 		}
 	}
+}
 </script>
 
 <style>
-	page {
-		height: auto;
-		min-height: 100%;
-	}
+page {
+	height: auto;
+	min-height: 100%;
+}
 
-	.uni-card {
-		box-shadow: none;
-	}
+.uni-card {
+	box-shadow: none;
+}
 
-	.uni-list:after {
-		height: 0;
-	}
+.uni-list:after {
+	height: 0;
+}
 
-	.uni-list:before {
-		height: 0;
-	}
+.uni-list:before {
+	height: 0;
+}
 </style>
---
+++ pages/table/table.vue
@@ -0,0 +1,442 @@
+<template>
+	<view class="wrap">
+		<view class="h1">简单table</view>
+		<view class="div-table">
+			<view class="tr">
+				<view class="td">1</view>
+				<view class="td">2</view>
+				<view class="td">3</view>
+			</view>
+			<view class="tr">
+				<view class="td">1</view>
+				<view class="td">2</view>
+				<view class="td">3</view>
+			</view>
+		</view>
+		<view class="h1">跨列table</view>
+		<view class="div-table">
+			<view class="tr">
+				<view class="td">1</view>
+				<view class="td">2</view>
+				<view class="td">3</view>
+			</view>
+			<view class="tr">
+				<view class="td noPadding">
+					<view class="celspan">
+						<view class="td">100</view>
+						<view class="td">100</view>
+					</view>
+				</view>
+				<view class="td">2</view>
+				<view class="td">3</view>
+			</view>
+		</view>
+		<view class="h1">跨行table</view>
+		<view class="div-table">
+			<view class="tr">
+				<view class="td">1</view>
+				<view class="td">2</view>
+				<view class="td">3</view>
+			</view>
+			<view class="tr">
+				<view class="td noPadding">
+					<view class="rowspan">
+						<view class="tr"><view class="td">100</view></view>
+						<view class="tr"><view class="td">100</view></view>
+					</view>
+				</view>
+				<view class="td">2</view>
+				<view class="td">3</view>
+			</view>
+		</view>
+
+		<view class="h1">有头部的table</view>
+		<view class="div-table">
+			<view class="thead">
+				<view class="th">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+			<view class="tr">
+				<view class="td">1</view>
+				<view class="td">2</view>
+				<view class="td">3</view>
+			</view>
+			<view class="tr">
+				<view class="td">1</view>
+				<view class="td">2</view>
+				<view class="td">3</view>
+			</view>
+			<view class="tr">
+				<view class="td">1</view>
+				<view class="td">2</view>
+				<view class="td">3</view>
+			</view>
+			<view class="tr">
+				<view class="td">1</view>
+				<view class="td">2</view>
+				<view class="td">3</view>
+			</view>
+		</view>
+		<view class="h1">有头部的和body的table</view>
+		<view class="div-table">
+			<view class="thead">
+				<view class="th">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+			<view class="tbody">
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+		</view>
+		<view class="h1">固定头部，body不变的table</view>
+		<view class="div-table fixed-thead">
+			<view class="thead">
+				<view class="th">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+			<view class="tbody">
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+		</view>
+
+		<view class="h1">有头部的，body,底部的table</view>
+		<view class="div-table">
+			<view class="thead">
+				<view class="th">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+			<view class="tbody">
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+			<view class="tfoot">
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+		</view>
+		<view class="h1">头尾固定，内容滚动table</view>
+		<view class="div-table fixed-thead">
+			<view class="thead">
+				<view class="th">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+			<view class="tbody">
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+			<view class="tfoot">
+				<view class="tr">
+					<view class="td">1</view>
+					<view class="td">2</view>
+					<view class="td">3</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss">
+.h1 {
+	text-align: left;
+	font-size: 30upx;
+	font-weight: bold;
+	padding: 10upx 0;
+}
+.wrap {
+	margin: 20upx;
+}
+$div-table-border-color: #666;
+$div-table-border-width: 1upx;
+
+.div-table {
+	display: table;
+	width: 100%;
+	height: 100%;
+	border: $div-table-border-width solid $div-table-border-color;
+	box-sizing: border-box;
+	table-layout: fixed;
+	position: relative;
+	.celspan {
+		display: table;
+		width: 100%;
+		height: 100%;
+		.td {
+			display: table-cell;
+			border: none !important;
+		}
+		.td + .td {
+			border-left: $div-table-border-width solid $div-table-border-color !important;
+		}
+	}
+	.rowspan {
+		display: table;
+		width: 100%;
+		height: 100%;
+		.tr {
+			display: table-row;
+			border: none !important;
+		}
+	}
+
+	&.fixed-thead {
+		.tbody,
+		.thead,
+		.tr,
+		.th,
+		.td {
+			display: block;
+		}
+		.tr,
+		.th {
+			&:after {
+				content: '';
+				display: block;
+				visibility: hidden;
+				clear: both;
+				height: 0;
+			}
+		}
+		.td {
+			float: left;
+			width: 33.33%;
+		}
+		.tbody {
+			height: 400upx;
+			overflow-y: auto;
+			overflow-x: hidden;
+		}
+	}
+
+	.tr,
+	.th {
+		display: table-row;
+		& + .tr,
+		& + .th {
+			.td,
+			.th {
+				border-top: $div-table-border-width solid $div-table-border-color;
+				word-break: break-word;
+			}
+		}
+	}
+	.rowspan {
+		.tr,
+		.th {
+			display: table-row;
+			.td,
+			.th {
+				border: none;
+			}
+			& + .tr,
+			& + .th {
+				.td,
+				.th {
+					border-top: $div-table-border-width solid $div-table-border-color;
+				}
+			}
+		}
+	}
+	.td {
+		display: table-cell;
+		vertical-align: middle;
+		text-align: center;
+		box-sizing: border-box;
+		padding: 10upx;
+		&.noPadding {
+			padding: 0;
+		}
+		& + .td {
+			border-left: $div-table-border-width solid $div-table-border-color;
+		}
+	}
+	.th .td {
+		font-weight: bold;
+	}
+	.tbody {
+		display: table-row-group;
+	}
+	.thead {
+		display: table-header-group;
+		.tr,
+		.th {
+			.td,
+			.th {
+				border-bottom: $div-table-border-width solid $div-table-border-color;
+			}
+		}
+	}
+	.tfoot {
+		display: table-footer-group;
+		.tr,
+		.th {
+			.td,
+			.th {
+				border-top: $div-table-border-width solid $div-table-border-color;
+			}
+		}
+	}
+	.colgroup {
+		display: table-column-group;
+	}
+	.col {
+		display: table-column;
+	}
+	.caption {
+		display: table-caption;
+	}
+}
+</style>
